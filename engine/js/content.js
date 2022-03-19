694bb4d2f245eea802dd39d342f623326f3ad499
Pranav Ramesh <laphatize@protonmail.com>
2022-03-19 14:51:24 -0400

    Loader for YT

--- engine/js/content.js
+++ engine/js/content.js
@@ -1,50 +1,67 @@
 // hi srihari and raj
 //yo nav
 
-window.addEventListener('load', function () {
+document.addEventListener('load', 
+  function() { 
+  //  window.alert("loaded")
+   
     // Fetch all the content on the page
-    console.log(document.body.innerText)
+    // console.log(document.body.innerText)
+  // this.window.alert("conversation")
+  if (window.location.href.split("/watch")[0] == "https://www.youtube.com") {
+    window.alert("first test passed")
+    setInterval(() => {   
+      try { 
+      this.window.alert("This youtube video is about" + document.getElementsByClassName('title style-scope ytd-video-primary-info-renderer')[0].innerText)
+      } catch(err) {
+        console.log(err);
+      }
+    }, 1000);
 
+  } else {
+    window.alert("test failed ")
+  }
+  var susDomains = ["https://www.reddit.com/", "https://youtube.com"];
+  //this.window.alert(this.window.location.href)
+  //this.window.alert(susDomains.includes(this.window.location.href))
 
-    var susDomains = ["https://www.reddit.com/", "https://youtube.com"];
-    //this.window.alert(this.window.location.href)
-    //this.window.alert(susDomains.includes(this.window.location.href))
+  if (susDomains.includes(this.window.location.href)) {
+      // this.document.write("Blocked");
+      // this.window.alert("Violation detected")
+      this.document.write(`
+          
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <title>👀</title>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+        </head>
+        <body>
+        <section class="hero is-light is-fullheight">
+        <div class="hero-body">
+          <div class="">
+            <p class="title">
+              You've got this Pranav!
+            </p>
+            <p class="subtitle">
+              You have NULL till your next break.
+            </p>
+            <button>Get back to work</button> <button>Dismiss</button>
 
-    if (susDomains.includes(this.window.location.href)) {
-        // this.document.write("Blocked");
-        // this.window.alert("Violation detected")
-        this.document.write(`
-            
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>👀</title>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
-          </head>
-          <body>
-          <section class="hero is-light is-fullheight">
-          <div class="hero-body">
-            <div class="">
-              <p class="title">
-                You've got this Pranav!
-              </p>
-              <p class="subtitle">
-                You have NULL till your next break.
-              </p>
-              <button>Get back to work</button> <button>Dismiss</button>
-
-              <p class="mt-6">&copy; Nudge 2022</p>
-            </div>
+            <p class="mt-6">&copy; Nudge 2022</p>
           </div>
-        </section>
-          </body>
-        </html>
-        
-        `)
-    } else {
-        // this.window.alert("Violation not detected")
-    }
+        </div>
+      </section>
+        </body>
+      </html>
+      
+      `)
+  } else {
+      // this.window.alert("Violation not detected")
+  }
 
-})
+
+
+  }, false);
